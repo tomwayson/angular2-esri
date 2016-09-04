7d8314b250e6321126dcf6d816410d63625dcc7a
edward pantoja <Macabeast@edwards-MacBook-Pro.local>
2016-09-04 15:20:52 -0700

    Modified config files for Angular2 RC6

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,12 +5,14 @@
   var map = {
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'boot.js',  defaultExtension: 'js' },
+    'angular2-in-memory-web-api': {main: './index.js',defaultExtension:'js'},
     'rxjs':                       { defaultExtension: 'js' },
     'esri':                       { defaultExtension: 'js' }
   };
@@ -21,9 +23,7 @@
     'http',
     'platform-browser',
     'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
+    'router'
   ];
   // Add package entries for angular packages
   ngPackageNames.forEach(function(pkgName) {
